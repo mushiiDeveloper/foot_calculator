1f732055cbfd42a8a7612fb863fde623a6ce72eb
Alex Halk <vovik.mushiy@gmail.com>
2018-07-23 19:29:37 +0300

    Next tests

--- spec/ingredient_quantity_spec.rb
+++ spec/ingredient_quantity_spec.rb
@@ -15,4 +15,31 @@ describe IngredientQuantity do
       it {expect(bad_ingredient.ingredient.class.name).not_to eq('Ingredient')}
     end
   end
+
+  describe '#+' do
+    let(:pomid) {Ingredient.new(name: 'Tomato', cost: 100)}
+    let(:pomid_quantity) {described_class.new(ingredient: pomid, quanity: 5)}
+    let(:pomid_quantity_second) {described_class.new(ingredient: pomid, quanity: 3)}
+
+    context 'when object are equally' do
+      it 'should return sum' do
+        expect(pomid_quantity.quanity).to eq(8)
+      end
+    end
+  end
+
+  describe '#*' do
+    let(:pomid) {Ingredient.new(name: 'Tomato', cost: 100)}
+    let(:pomid_quantity) {described_class.new(ingredient: pomid, quanity: 5)}
+    context 'when happens multiplication' do
+      it 'should return good result' do
+        expect(pomid_quantity.*(20)).to eq(pomid_quantity.quanity * 20)
+      end
+
+      it 'should be bad result' do
+        expect(number.to_f != number || number.to_i != number).to
+        raise_error("Wrong parameters")
+      end
+    end
+  end
 end
--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -7,15 +7,13 @@ describe Ingredient do
 
     context 'when ingridients are equal' do
       it 'they are trule equal' do
-        expect(pomid.name).to eq(tomato.name)
-        expect(pomid.cost).to eq(tomato.cost)
+        expect(pomid).to eq(tomato)
       end
     end
 
     context 'when ingridients are not equal' do
       it 'Cost is different' do
-        expect(pomid.name).to eq(tomato.name)
-        expect(pomid.cost).not_to eq(bet.cost)
+        expect(pomid).not_to eq(bet)
       end
     end
   end
